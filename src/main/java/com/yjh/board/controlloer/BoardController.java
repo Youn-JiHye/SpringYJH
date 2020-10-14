da8a0429fbc3a386ba7f6aa262d067d878a3ab3f
Youn-JiHye <3455jih@gmail.com>
2020-10-14 15:04:36 +0900

    Spring gradle project 로 변경

---
+++ src/main/java/com/yjh/board/controlloer/BoardController.java
@@ -0,0 +1,79 @@
+package com.yjh.board.controlloer;
+
+import com.yjh.board.dto.BoardDto;
+import com.yjh.board.service.BoardService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class BoardController {
+    private BoardService boardService;
+
+    /* 게시글 목록 */
+    @GetMapping("/")
+    public String list(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
+        List<BoardDto> boardList = boardService.getBoardlist(pageNum);
+        Integer[] pageList = boardService.getPageList(pageNum);
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("pageList", pageList);
+
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String write(){
+        return "board/write.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto){
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no,Model model){
+        BoardDto boardDto=boardService.getPost(no);
+
+        model.addAttribute("boardDto",boardDto);
+        return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model){
+        BoardDto boardDto=boardService.getPost(no);
+
+        model.addAttribute("boardDto",boardDto);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDto){
+        boardService.savePost(boardDto);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no){
+        boardService.deletePost(no);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/board/search")
+    public String search(@RequestParam(value = "keyword") String keyword,Model model){
+        List<BoardDto> boardDtoList=boardService.searchPosts(keyword);
+
+        model.addAttribute("boardList", boardDtoList);
+
+        return "board/list.html";
+    }
+}
